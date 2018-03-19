fbc73701569c2ec2dfbb969a2bff57371b4530c2
unknown <distancein1997@gmail.com>
2018-03-19 20:04:21 +0800

    solution

--- lesson4/PermCheck.py
+++ lesson4/PermCheck.py
@@ -47,3 +47,14 @@
 # expected worst-case time complexity is O(N);
 # expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 
+def solution(A):
+    reference = [0 for i in range(len(A))]
+    for i in A:
+        if i > len(A):
+            return 0
+        else:
+            if reference[i - 1] == 0:
+                reference[i - 1] = 1
+            else:
+                return 0
+    return 1
