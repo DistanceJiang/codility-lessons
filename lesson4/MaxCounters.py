e51a68d8e6dbb147cfea6398436ec3527b89208b
unknown <distancein1997@gmail.com>
2018-03-19 21:27:58 +0800

    solution

--- lesson4/MaxCounters.py
+++ lesson4/MaxCounters.py
@@ -58,3 +58,28 @@
 # expected worst-case time complexity is O(N+M);
 # expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 
+from random import random
+
+a = [3, 4, 4, 6, 1, 4, 4]
+b = list(int(100 * random()) for i in range(100))
+
+def solution(N, A):
+    counters = {}
+    max_recorder = 0
+    for i in A:
+        if 1 <= i <= N:
+            if i in [int(j) for j in counters.keys()]:
+                counters['{0}'.format(i)] += 1
+            else:
+                counters['{0}'.format(i)] = 1
+        elif i == N + 1 and len(counters.values()) != 0:
+            max_recorder += max(counters.values())
+            counters = {}
+    result = [0 for i in range(N)]
+    for i in range(N):
+        if i + 1 in [int(j) for j in counters.keys()]:
+            result[i] = counters['{0}'.format(i + 1)] + max_recorder
+        else:
+            result[i] = max_recorder
+    return result
+    
