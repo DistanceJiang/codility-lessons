82c891d606df12d4ee694c1943a7dea4894f6efa
unknown <distancein1997@gmail.com>
2018-03-21 20:45:08 +0800

    the 100% performance solution

--- lesson5/GenomicRangeQuery.py
+++ lesson5/GenomicRangeQuery.py
@@ -48,10 +48,28 @@ P = [2, 5, 0]
 Q = [4, 5, 6]
 
 def solution(S, P, Q):
-    reference = {'A':1, 'C':2, 'G':3, 'T':4}
-    transposed = [reference[i] for i in S]
+    transposed = []
+    for i in S:
+        if i == 'A':
+            transposed.append([1, 0, 0, 0])
+        elif i == 'C':
+            transposed.append([0, 1, 0, 0])
+        elif i == 'G':
+            transposed.append([0, 0, 1, 0])
+        else:
+            transposed.append([0, 0, 0, 1])
+    for i in range(len(transposed) - 1):
+        for j in range(4):
+            transposed[i + 1][j] += transposed[i][j]
     result = []
     for i in range(len(P)):
-        sample = transposed[P[i]:Q[i] + 1]
-        result.append(min(sample))
+        x = P[i]
+        y = Q[i]
+        sub = 0
+        for j in range(4):
+            if x >= 1:
+                sub = transposed[x - 1][j]
+            if transposed[y][j] - sub > 0:
+                result.append(j + 1)
+                break
     return result
