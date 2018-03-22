cde4d853bd95701be6b5b9d1a71d2cb8df68e153
unknown <distancein1997@gmail.com>
2018-03-22 22:57:00 +0800

    finally get the solution

--- lesson5/MinAvgTwoSlice.py
+++ lesson5/MinAvgTwoSlice.py
@@ -1,44 +1,47 @@
-# A non-empty zero-indexed array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
+from random import random
 
-# For example, array A such that:
+a = [int(100 * random()) for i in range(30)]
 
-#     A[0] = 4
-#     A[1] = 2
-#     A[2] = 2
-#     A[3] = 5
-#     A[4] = 1
-#     A[5] = 5
-#     A[6] = 8
-# contains the following example slices:
+def solution(A):
+    p = prefix_sums(A)
+    n = len(A)
+    min_idx = 0
+    min_value = 10001
 
-# slice (1, 2), whose average is (2 + 2) / 2 = 2;
-# slice (3, 4), whose average is (5 + 1) / 2 = 3;
-# slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
-# The goal is to find the starting position of a slice whose average is minimal.
+    for i in range(n - 1):
+        if i == 0:
+            if p[1] / 2 < min_value:
+                min_idx = 0
+                min_value = p[1] / 2
+            if len(p) > 2 and p[2] / 3 < min_value:
+                min_idx = 0
+                min_value = p[2] / 3
+        else:
+            if (p[i + 1] - p[i - 1]) / 2 < min_value:
+                min_idx = i
+                min_value = (p[i + 1] - p[i - 1]) / 2
+                print(min_idx, min_value)
+            if i < n - 2 and (p[i + 2] - p[i - 1]) / 3 < min_value:
+                min_idx = i 
+                min_value = (p[i + 2] - p[i - 1]) / 3
+                print(min_idx, min_value)
+    return min_idx
 
-# Write a function:
+def prefix_sums(a):
+    n = len(a)
+    p = [0] * n
+    for i in range(n):
+        if i == 0:
+            p[i] = a[i]
+        else:
+            p[i] = a[i] + p[i - 1]
+    return p
 
-# def solution(A)
-
-# that, given a non-empty zero-indexed array A consisting of N integers, returns the starting position of the slice with the minimal average. If there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
-
-# For example, given array A such that:
-
-#     A[0] = 4
-#     A[1] = 2
-#     A[2] = 2
-#     A[3] = 5
-#     A[4] = 1
-#     A[5] = 5
-#     A[6] = 8
-# the function should return 1, as explained above.
-
-# Assume that:
-
-# N is an integer within the range [2..100,000];
-# each element of array A is an integer within the range [−10,000..10,000].
-# Complexity:
-
-# expected worst-case time complexity is O(N);
-# expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
-
+# Every slice must be of size two or three. 
+# Slices of bigger sizes are created from such smaller slices. 
+# Therefore should any bigger slice have an optimal value, 
+# all sub-slices must be the same, for this case to hold true. 
+# Should this not be true, one of the sub-slices must be the optimal slice. 
+# The others being bigger. Therefore we check all possible slices of size 2/3 
+# and return the smallest one. The first such slice is the correct one, do not use <=!
+# The description above are copied from https://www.martinkysel.com/codility-minavgtwoslice-solution/
